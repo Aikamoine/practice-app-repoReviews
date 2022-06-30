c1d1830f81cbc7a14a6e14eb8d61744c66d3e5a0
Aikamoine <atte.laakso@helsinki.fi>
2022-06-30 17:22:20 +0300

    part 10 exercises up to 10.8

---
+++ src/components/FormikTextInput.jsx
@@ -0,0 +1,45 @@
+import { StyleSheet, View } from 'react-native';
+import { useField } from 'formik';
+
+import TextInput from './TextInput';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  errorText: {
+    marginTop: 5,
+  },
+  input: {
+    width: 300,
+    height: 40,
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 15, 
+    fontSize: 16,
+    margin: 10
+  },
+  
+});
+
+const FormikTextInput = ({ name, ...props }) => {
+  const [field, meta, helpers] = useField(name);
+  const showError = meta.touched && meta.error;
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        onChangeText={value => helpers.setValue(value)}
+        onBlur={() => helpers.setTouched(true)}
+        value={field.value}
+        error={showError}
+        {...props}
+      />
+      {showError && <Text style={styles.errorText}>{meta.error}</Text>}
+    </View>
+  );
+};
+
+export default FormikTextInput;
