9190d7b6b2f579e659cd100fc8a819bfe051babb
Aikamoine <atte.laakso@helsinki.fi>
2022-06-30 09:23:47 +0300

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Words n shit</Text>
+      <Text>Words n stuff</Text>
       <StatusBar style="auto" />
     </View>
   );
